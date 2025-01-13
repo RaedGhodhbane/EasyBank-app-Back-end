aab8f0334a4b81a052b61d314377f30c67a3b258
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-01-13 20:15:25 +0100

    Implementing Circuit Breaker pattern in Gateway - Part 2

--- gatewayserver/src/main/java/com/easybank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/easybank/gatewayserver/GatewayserverApplication.java
@@ -22,7 +22,8 @@ public class GatewayserverApplication {
 						   .path("/easybank/accounts/**")
 						   .filters(f -> f.rewritePath("/easybank/accounts/(?<segment>.*)","/${segment}")
 								   .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-								   .circuitBreaker(config -> config.setName("accountsCircuitBreaker")))
+								   .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										   .setFallbackUri("forward:/contactSupport")))
 								.uri("lb://ACCOUNTS"))
 						.route(p -> p
 						.path("/easybank/cards/**")
