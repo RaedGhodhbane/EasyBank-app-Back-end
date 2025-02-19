23ab21fb274159f177fa6b61de2d609e405e9509
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-02-19 18:45:09 +0100

    Observability and monitoring of microservices

--- accounts/src/main/java/com/easybank/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/easybank/accounts/controller/CustomerController.java
@@ -58,8 +58,9 @@ public class CustomerController {
                                                                        @RequestParam
                                                                        @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
                                                                    String mobileNumber) {
-        logger.debug("easyBank-correlation-id found: {}", correlationId);
+        logger.debug("fetchCustomerDetails method start");
         CustomerDetailsDTO customerDetailsDTO = iCustomerService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails method end");
         return ResponseEntity.status(HttpStatus.OK)
                 .body(customerDetailsDTO);
     }
--- loans/src/main/java/com/easybank/loans/controller/LoansController.java
+++ loans/src/main/java/com/easybank/loans/controller/LoansController.java
@@ -104,8 +104,9 @@ import org.springframework.web.bind.annotation.*;
                                                          @RequestParam
                                                          @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                          String mobileNumber) {
-            logger.debug("easyBank-correlation-id found: {}", correlationId);
+            logger.debug("fetchLoanDetails method start");
             LoansDTO loansDto = iLoansService.fetchLoan(mobileNumber);
+            logger.debug("fetchLoanDetails method end");
             return ResponseEntity.status(HttpStatus.OK).body(loansDto);
         }
 
